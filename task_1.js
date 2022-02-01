509d492acfa18858b6be24d954e030f5d3cdb668
TheRashRash <TheRashRash@github.com>
2022-02-01 10:16:01 +0000

    Initial commit

---
+++ task_1.js
@@ -0,0 +1,15 @@
+`use strict`
+
+console.log("Rasheed");
+console.log("Gudal");
+console.log("London");
+console.log("Dunno reaally");
+
+let car_make = "Tesla"
+let car_model = "model S"
+
+console.log("My favourite car is " + car_make + " and the model is " + car_model);
+
+console.log("%c Some message here ","color: orange; font-family: fantasy; text-weight: bold; background-color: black; padding: 10px;");
+
+
