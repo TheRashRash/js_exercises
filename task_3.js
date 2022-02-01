b308005bcc5da2951d72f1e41b8a76f15b5e39bb
TheRashRash <TheRashRash@github.com>
2022-02-01 14:40:42 +0000

    conditional exercise

--- task_3.js
+++ task_3.js
@@ -19,23 +19,51 @@ for(let a = 100; a <= 200; a++){
 //    }
 //}
 
-let now = new Date();
-let day = now.getDay();
+// let now = new Date();
+// let day = now.getDay();
 
-switch(day) {
-    case 0:
-        console.log(`Sunday`);
-        break;
-      case 1:
-        console.log(`Saturday`);
-        break;
-      case 2:
-        console.log(`Monday`);
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-      default:
-        console.log(`Please enter a valid range.`)
-      // code block
+// switch(day) {
+//     case 0:
+//         console.log(`Sunday`);
+//         break;
+//       case 1:
+//         console.log(`Saturday`);
+//         break;
+//       case 2:
+//         console.log(`Monday`);
+//       case 3:
+//       case 4:
+//       case 5:
+//       case 6:
+//       default:
+//         console.log(`Please enter a valid range.`)
+//       // code block
+//   }
+
+age = 0;
+
+if (18 <= age <= 65) {
+    console.log("Age is in range");
+  } else if (age < 18) {
+    console.log("Underage");
+  } else {
+    console.log("Catch all else");
   }
+
+// switch (age) {
+//     case ((age == true) && (18 <= age <= 65)):
+//         console.log("You are the right age!");
+//         break;
+//     case ((age == true) && !(18 <= age )):
+//         console.log("You are underage!");
+//         break;
+//     case ((age == true) && !(65 >= age)):
+//         console.log("You are unfortunately in the wrong category...");
+//         break;
+//     case (age == false):
+//         console.log("No age.");
+//         break;
+
+//     }   
+
+let x = age >= 50 ? 'You are over 50' : "You are under 50";
